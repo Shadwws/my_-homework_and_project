f3cc849c8ea1df06dd74ca5aefb75a7d39999f58
Martín_ab <leonelguajardo55@gmail.com>
2023-08-14 21:42:11 -0300

    clase practica

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -2,23 +2,48 @@
 
 float centigrados_a_fahrenheit(float centigrados)
 {
-    float farenheit=(centigrados*9/5)+32;
-    return(0);
+    float resultado_fahrenheit=centigrados*1.8+32;
+    return(resultado_fahrenheit);
 }
 
 float fahrenheit_a_centigrados(float fahrenheit)
 {
-    float centigrados=(fahrenheit-32)*5/9;
-    return(0);
+    float resultado_centigrados=(fahrenheit-32)*5/9;
+    return(resultado_centigrados);
 }
 
 int main()
 {
-    variable=0;
-    scanf(variable);
-    centigrados_a_fahrenheit(variable);
-    fahrenheit_a_centigrados(variable);
-    printf("%f \n"centigrados_a_fahrenheit);
-    printf("%f \n"fahrenheit_a_centigrados);
-    return(0);
+	float resultado_centigrados;
+	float resultado_fahrenheit;
+	float fahrenheits;
+	float centigrados;
+	int variable;
+	printf("ingrese 0 para centigrados a fahrenheit \n");
+	printf("ingrese 1 para fahrenheit a centigrados \n");
+	scanf("%d \n", &variable);
+
+	if(variable==0)
+	{
+		printf("ingrese los centigrados a convertir: ");
+		scanf("%f" ,centigrados);
+		centigrados_a_fahrenheit(centigrados);
+		printf("%f", resultado_centigrados);
+	}
+
+	else if(variable==1)
+	{
+		printf("Ingrese valor a convertir");
+		scanf("%f", &fahrenheits);
+		fahrenheit_a_centigrados(fahrenheits);
+		printf("%f \n", resultado_fahrenheit);
+
+	}
+
+	else
+	{
+		printf("valor erroneo");
+	}
+	return(0);
+
 }
